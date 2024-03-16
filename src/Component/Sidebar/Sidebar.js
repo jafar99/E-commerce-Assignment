f44f431df0f2ca099df8d8b2ca56cf00a33b938f
jafar <jafaralihajari@gmail.com>
2024-03-16 19:58:36 +0530

    added active tab

--- src/Component/Sidebar/Sidebar.js
+++ src/Component/Sidebar/Sidebar.js
@@ -1,25 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "./Sidebar.css";
 
 const Sidebar = () => {
+  const location = useLocation();
+  const [selectedSubMenu, setSelectedSubMenu] = useState(null);
+
+  // Function to handle submenu selection
+  const handleSubMenuSelect = (submenu) => {
+    setSelectedSubMenu(submenu);
+  };
+
   return (
     <div className="sidebar">
       <ul>
         <li>
           <Link to="/">Dashboard</Link>
         </li>
+        {selectedSubMenu && (
+          <div className="selected-submenu">{selectedSubMenu}</div>
+        )}
         <li>
           <Link to="/electronics">Electronics</Link>
           <ul className="submenu">
             <li>
-              <Link to="/electronics/mobile">Mobile</Link>
+              <Link
+                to="/electronics/mobile"
+                onClick={() => handleSubMenuSelect("Mobile")}
+              >
+                Mobile
+              </Link>
             </li>
             <li>
-              <Link to="/electronics/earphones">Earphones</Link>
+              <Link
+                to="/electronics/earphones"
+                onClick={() => handleSubMenuSelect("Earphones")}
+              >
+                Earphones
+              </Link>
             </li>
             <li>
-              <Link to="/electronics/mac">Mac</Link>
+              <Link
+                to="/electronics/mac"
+                onClick={() => handleSubMenuSelect("Mac")}
+              >
+                Mac
+              </Link>
             </li>
           </ul>
         </li>
@@ -27,13 +53,28 @@ const Sidebar = () => {
           <Link to="/clothing">Clothing</Link>
           <ul className="submenu">
             <li>
-              <Link to="/clothing/tshirt">Tshirt</Link>
+              <Link
+                to="/clothing/tshirt"
+                onClick={() => handleSubMenuSelect("Tshirt")}
+              >
+                Tshirt
+              </Link>
             </li>
             <li>
-              <Link to="/clothing/shirt">Shirts</Link>
+              <Link
+                to="/clothing/shirt"
+                onClick={() => handleSubMenuSelect("Shirts")}
+              >
+                Shirts
+              </Link>
             </li>
             <li>
-              <Link to="/clothing/jeans">Jeans</Link>
+              <Link
+                to="/clothing/jeans"
+                onClick={() => handleSubMenuSelect("Jeans")}
+              >
+                Jeans
+              </Link>
             </li>
           </ul>
         </li>
